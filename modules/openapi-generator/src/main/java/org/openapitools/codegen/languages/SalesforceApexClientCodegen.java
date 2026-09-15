b56617fe12ec31d5e97725fafedcb9260c5755ee
David Lappert <dclappert@gmail.com>
2026-09-15 00:49:59 +0000

    refactor(salesforce-apex): reorganize imports in SalesforceApexClientCodegen.java

--- modules/openapi-generator/src/main/java/org/openapitools/codegen/languages/SalesforceApexClientCodegen.java
+++ modules/openapi-generator/src/main/java/org/openapitools/codegen/languages/SalesforceApexClientCodegen.java
@@ -1,8 +1,24 @@
 package org.openapitools.codegen.languages;
 
-import io.swagger.v3.oas.models.media.Schema;
+import static org.openapitools.codegen.utils.CamelizeOption.LOWERCASE_FIRST_LETTER;
+import static org.openapitools.codegen.utils.StringUtils.camelize;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Locale;
+
 import org.apache.commons.lang3.StringUtils;
-import org.openapitools.codegen.*;
+import org.openapitools.codegen.CliOption;
+import org.openapitools.codegen.CodegenModel;
+import org.openapitools.codegen.CodegenOperation;
+import org.openapitools.codegen.CodegenParameter;
+import org.openapitools.codegen.CodegenProperty;
+import org.openapitools.codegen.CodegenType;
+import org.openapitools.codegen.DefaultCodegen;
+import org.openapitools.codegen.IJsonSchemaValidationProperties;
+import org.openapitools.codegen.SupportingFile;
 import org.openapitools.codegen.model.ModelMap;
 import org.openapitools.codegen.model.ModelsMap;
 import org.openapitools.codegen.model.OperationsMap;
@@ -10,13 +26,7 @@ import org.openapitools.codegen.utils.ModelUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.File;
-import java.util.*;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
-import static org.openapitools.codegen.utils.CamelizeOption.LOWERCASE_FIRST_LETTER;
-import static org.openapitools.codegen.utils.StringUtils.camelize;
+import io.swagger.v3.oas.models.media.Schema;
 
 public class SalesforceApexClientCodegen extends DefaultCodegen {
 
